ac7bdaabccdca3d3c413271da8dec1a293efafd5
PotapovAndr <potapovandr@gmail.com>
2024-10-21 21:05:11 +0900

    Задание 2

--- Main.py
+++ Main.py
@@ -23,6 +23,31 @@ def read_recipes(file_name):
     
     return cook_book
 
+def get_shop_list_by_dishes(dishes, person_count, cook_book):
+    shop_list = {}
+    
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingredient_name = ingredient['ingredient_name']
+                quantity = ingredient['quantity'] * person_count
+                measure = ingredient['measure']
+                
+                if ingredient_name in shop_list:
+                    shop_list[ingredient_name]['quantity'] += quantity
+                else:
+                    shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
+        else:
+            print(f"Блюдо '{dish}' отсутствует в книге рецептов.")
+    
+    return shop_list
+
 file_name = 'recipes.txt'
 cook_book = read_recipes(file_name)
 print(cook_book)
+
+# Пример
+dishes = ['Запеченный картофель', 'Омлет']
+person_count = 2
+shop_list = get_shop_list_by_dishes(dishes, person_count, cook_book)
+print(shop_list)
